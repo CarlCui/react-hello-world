776a81c290647450c7fe484373a1e5bec9b8c449
Carl Cui <carlcui@builddirect.com>
2017-05-23 16:55:09 -0700

    use a more legit initial source code

--- src/reducers/sourceCode.ts
+++ src/reducers/sourceCode.ts
@@ -3,7 +3,7 @@ import {Reducer, Action} from 'redux';
 import {MODIFY_SOURCE_CODE} from '../constants';
 import {ModifySourceCodeAction} from '../actions';
 
-const INITIAL_STATE = '123';
+const INITIAL_STATE = 'var a = 123;';
 
 export const sourceCodeReducer: Reducer<string> = (state = INITIAL_STATE, action: Action) => {
     switch(action.type) {
